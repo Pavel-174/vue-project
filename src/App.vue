5cb790232b0041137a9991e36d4d906e20c85e7b
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-08-08 11:53:39 +0500

    отрисовка карточек из массива

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-    <div> Hello world! </div>
-    <div> Лайки: <strong> {{ likes }} </strong> </div>
-    <div> Дизлайки: <strong> {{ dislikes }} </strong> </div>
     <div>
-        <button v-on:click="addLike">Like</button>
-        <button @:click="addDislike">Dislike</button>
+        <div class='post' v-for="post in posts">
+            <div> <strong> Название: </strong> {{ post.title }} </div>
+            <div> <strong> Описание: </strong> {{ post.body }} </div>
+        </div>
     </div>
 </template>
 
@@ -12,21 +11,30 @@
     export default {
         data () {
             return {
-                likes: 0,
-                dislikes: 0,
+                posts: [
+                    { id: 1, title: "JavaScript", body: "JavaScript универсальный язык програмирования" },
+                    { id: 2, title: "JavaScript 1", body: "JavaScript универсальный язык програмирования 1" },
+                    { id: 3, title: "JavaScript 2", body: "JavaScript универсальный язык програмирования 2" },
+                ]
             }
         },
 
         methods: {
-            addLike (){
-                this.likes += 1;
-            },
-            addDislike (){
-                this.dislikes += 1;
-            }
         },
     }
     
 </script>
 
-<style></style>
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.post {
+    padding: 15px;
+    border: 3px solid teal;
+    margin-top: 15px;
+}
+</style>
