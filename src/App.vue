fe5338cb9843cf6069650aa848ee7e8db7076830
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-08-08 11:18:52 +0500

    like, dislike

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
 <template>
-    <div>Hello world!</div>
+    <div> Hello world! </div>
+    <div> Лайки: <strong> {{ likes }} </strong> </div>
+    <div> Дизлайки: <strong> {{ dislikes }} </strong> </div>
+    <div>
+        <button v-on:click="addLike">Like</button>
+        <button v-on:click="addDislike">Dislike</button>
+    </div>
 </template>
 
-<script></script>
+<script>
+    export default {
+        data () {
+            return {
+                likes: 0,
+                dislikes: 0,
+            }
+        },
+
+        methods: {
+            addLike (){
+                this.likes += 1;
+            },
+            addDislike (){
+                this.dislikes += 1;
+            }
+        },
+    }
+    
+</script>
 
 <style></style>
