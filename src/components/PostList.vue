4a8a73da7d012a88459685f3157eb893b8ce34b3
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-09-21 19:40:08 +0500

    transition group

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
     <div v-if="posts.length > 0">
         <h3>Список постов</h3>
-        <PostItem  
-          v-for="post in posts"
-          :post="post"
-          :key="post.id"
-          @remove="$emit('remove', post)"
-        />
+        <transition-group name="post-list">
+            <PostItem  
+              v-for="post in posts"
+              :post="post"
+              :key="post.id"
+              @remove="$emit('remove', post)"
+            />
+        </transition-group>
     </div>
     <h3 
       class="posts__header"
@@ -33,5 +35,22 @@ export default {
 <style scoped>
 .posts__header {
     color: red;
+}
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 1s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.post-list-move {
+  transition: transform 0.8s ease;
 }
 </style>
